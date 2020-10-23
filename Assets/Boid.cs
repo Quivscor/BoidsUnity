44e288629bc4562239dc2b3b743e9a3362245760
Quivscor <quivscor@gmail.com>
2020-10-23 14:16:36 +0200

    Project

---
+++ Assets/Boid.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boid : MonoBehaviour
+{
+    private AvoidanceDetector avoidanceDetector;
+    private CohesionDetector cohesionDetector;
+
+    private void Awake()
+    {
+        avoidanceDetector = GetComponentInChildren<AvoidanceDetector>();
+        cohesionDetector = GetComponentInChildren<CohesionDetector>();
+    }
+}
